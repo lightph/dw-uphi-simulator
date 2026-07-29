fc234ff1c5cac5339a2187eaf01b6defc144f977
lightph <luzperezhr@.gmail.com>
2026-07-28 22:31:06 -0300

    Add dummy executable for compile_commands generation

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "fftw_handler.hpp"  // Or whatever you named the header
+#include "types.h"
+
+int main() {
+    std::cout << "Domain Wall Simulator initialized.\n";
+    return 0;
+}
